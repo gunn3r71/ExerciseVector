388f13348a5dd2a1261ec309a8cc87dafa17f8bc
Lucas <lucasgunner3@gmail.com>
2021-11-21 17:07:54 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciseVector/Program.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using System;
+
+namespace ExerciseVector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(2, 3);
+            Vector v2 = new Vector(4, 5);
+            Vector v3 = v1 + v2;
+            Vector v4 = v3 * 3;
+
+            Console.WriteLine($"Vector: ({v4.X}, {v4.Y})");
+        }
+    }
+}
---
+++ ExerciseVector/Vector.cs
@@ -0,0 +1,18 @@
+﻿namespace ExerciseVector
+{
+    public struct Vector
+    {
+        public Vector(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public static Vector operator +(Vector a, Vector b) => new(a.X + b.X, a.Y + b.Y);
+
+        public static Vector operator *(Vector a, int m) => new(a.X * m, a.Y * m);
+    }
+}
