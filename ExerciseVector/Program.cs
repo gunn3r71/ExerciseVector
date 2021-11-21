79900f9f812cb9471050b9b489753144d3fff3c6
Lucas <lucasgunner3@gmail.com>
2021-11-21 17:08:19 -0300

    refatorando código

--- ExerciseVector/Program.cs
+++ ExerciseVector/Program.cs
@@ -7,8 +7,8 @@ namespace ExerciseVector
     {
         static void Main(string[] args)
         {
-            Vector v1 = new Vector(2, 3);
-            Vector v2 = new Vector(4, 5);
+            Vector v1 = new(2, 3);
+            Vector v2 = new(4, 5);
             Vector v3 = v1 + v2;
             Vector v4 = v3 * 3;
 
